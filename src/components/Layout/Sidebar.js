617da47063f1e20d14ced0f6bd513ac606273457
Gamel James <gamel@meezyart.com>
2020-01-30 19:13:30 -0800

    SIdebar Updates

--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -8,39 +8,40 @@ import {
 
 export const Sidebar = () => {
   return (
-    <div className="sidebar" testid="sidebar">
+    <div className="sidebar" data-testid="sidebar">
       <ul className="sidebar__generic">
-        <li>
-          <span>
-            <FaInbox />
-          </span>
-          <span>Inbox</span>
+        <li data-testid="inbox" className="inbox">
+          <div>
+            <span>
+              <FaInbox />
+            </span>
+            <span>Inbox</span>
+          </div>
         </li>
-        <li>
-          <span>
-            <FaRegCalendar />
-          </span>
-          <span>Today</span>
+        <li data-testid="today" className="today">
+          <div>
+            <span>
+              <FaRegCalendar />
+            </span>
+            <span>Today</span>
+          </div>
         </li>
-        <li>
-          <span>
-            <FaRegCalendarAlt />
-          </span>
-          <span>Next 7 Days</span>
+        <li data-testid="next_7" className="next_7">
+          <div>
+            <span>
+              <FaRegCalendarAlt />
+            </span>
+            <span>Next 7 Days</span>
+          </div>
         </li>
       </ul>
-
       <div className="sidebar__middle">
-        <span>
-          <FaChevronDown />
-        </span>
-        <h2>Projects</h2>
+          <span>
+            <FaChevronDown />
+          </span>
+          <h2>Projects</h2>
       </div>
-
-      <ul className="sidebar__projects">
-        projects will be here!
-      </ul>
-
+      <ul className="sidebar__projects">projects will be here!</ul>
       TODO add project component here
     </div>
   );
